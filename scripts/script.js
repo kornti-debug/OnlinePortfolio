ffe9cd5075b26b25f756c2bac16f2599ff1d3da9
kornti-debug <cc241070@fhstp.ac.at>
2025-02-20 18:21:01 +0100

    update image slider, images, projects

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,39 @@
 // Get menu toggle button (for mobile) and all navigation links
 const menuToggle = document.getElementById("menu-toggle");
 const menuLinks = document.querySelectorAll(".menu-link, .nav-ul_menu a");
+const slider = document.querySelector(".project-slider");
+const prevArrow = document.getElementById("arrow-previous-slider");
+const nextArrow = document.getElementById("arrow-next-slider");
+const slides = document.querySelectorAll('.project-slider img');
+const dots = document.querySelectorAll('.slider-nav a');
+const projectTitle = document.getElementById('project-title');
+
+nextArrow.addEventListener('click', ()=>{
+  slider.scrollBy({left: slider.clientWidth, behavior:'smooth'});
+});
+
+prevArrow.addEventListener('click', ()=>{
+  slider.scrollBy({left: -slider.clientWidth, behavior:'smooth'});
+});
+
+function updateActiveDot() {
+  let scrollLeft = slider.scrollLeft;
+  let slideWidth = slider.clientWidth;
+  let activeIndex = Math.round(scrollLeft / slideWidth); // Find the closest slide index
+
+  dots.forEach((dot, index) => {
+      dot.classList.toggle('active', index === activeIndex);
+  });
+
+  projectTitle.textContent = slides[activeIndex].alt;
+}
+
+    // Detect scrolling to update active dot
+    slider.addEventListener('scroll', updateActiveDot);
+
+    // Initialize the active dot on page load
+    updateActiveDot();
+
 
 // Function to highlight the active link
 function updateActiveLink() {
